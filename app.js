b36327a96f73005da543076d65819fb6a35e7b1c
Lewis <lewis.burgess314@gmail.com>
2025-04-09 18:05:58 +0100

    Release is fixed, version 3, Condensed workflows to sinlge deployment pipeline

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 app.listen(PORT, () => {
-  this_causes_error
+  // this_causes_error
   console.log(`server started on port ${PORT}`) // eslint-disable-line
 })
