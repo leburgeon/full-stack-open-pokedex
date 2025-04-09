0abf4f5ef126a363f20d1803d17144e394699127
Lewis <lewis.burgess314@gmail.com>
2025-04-09 11:57:25 +0100

    Adds test for ensuring a specific pokemon page can be visited

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -5,5 +5,11 @@ test.describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('ivysaurs page can be navigated to', async ({page}) => {
+    const childOfLink = page.getByText('ivysaur')
+    const ivysaurLink = page.getByRole('link').filter({has: childOfLink})
+    await ivysaurLink.press()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
