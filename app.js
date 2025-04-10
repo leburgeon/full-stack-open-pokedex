a758033710dc4519fb7125b36507ecfd1265d43e
Lewis <lewis.burgess314@gmail.com>
2025-04-10 08:46:22 +0100

    fixing change

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
-app.listen(PORT + 1, () => {
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line
 })
