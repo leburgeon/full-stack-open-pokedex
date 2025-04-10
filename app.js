860d3907906f3a35d812a9d078bc0e85c270f815
leburgeon <116656845+leburgeon@users.noreply.github.com>
2025-04-10 17:23:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('6')
+  res.send('7')
 })
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK')
 })
 
+app.get('pokemon', (req, res) => {
+  res.status(200).send('r cool')
+})
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line
 })
