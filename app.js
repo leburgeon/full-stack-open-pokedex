45550b0ea4f7c9d75c0f054b344ebee3ce4dda1c
Lewis <lewis.burgess314@gmail.com>
2025-04-11 10:04:57 +0100

    Great push

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK')
 })
 
-app.get('pokemon', (req, res) => {
+app.get('/pokemon', (req, res) => {
   res.status(200).send('r cool')
 })
 app.listen(PORT, () => {
