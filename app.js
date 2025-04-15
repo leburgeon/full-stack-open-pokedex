588406388935ef1a592d274cd8eeb848e0e06911
Lewis <lewis.burgess314@gmail.com>
2025-04-15 16:51:37 +0100

    fixed breaking code

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('18')
-  awawassss
+  res.send('19')
 })
 
 app.get('/health', (req, res) => {
